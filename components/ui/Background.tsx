d945bec5088345e459b2f5a97223344f159f53e4
Dharmesh <raikwardharmesh67@gmail.com>
2024-12-24 01:47:26 +0530

    full app push

---
+++ components/ui/Background.tsx
@@ -0,0 +1,11 @@
+import Spline from '@splinetool/react-spline/next';
+
+export default function BG() {
+  return (
+    <main className='h-screen w-screen'>
+      <Spline
+        scene="https://prod.spline.design/ZD8CXyVdGChd3b7H/scene.splinecode" 
+        />
+    </main>
+  );
+}
